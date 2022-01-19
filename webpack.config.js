3d1f79b0400b09952d4e894283145ff76ff130c6
wdoconnell <91283923+wdoconnell@users.noreply.github.com>
2022-01-19 13:18:30 -0500

    updated to add .tsx processing rule

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,11 @@ const config = {
   devtool: 'source-map',
   module: {
     rules: [
+      {
+        test: /\.tsx?/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
       { test: /\.js$/,
         enforce: 'pre',
         use: ['source-map-loader'],
