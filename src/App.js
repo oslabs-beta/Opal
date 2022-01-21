8780acae45cd3459e22556ebb0e4bf68d2d9792c
Hussein Hamade <h.hamade98@gmail.com>
2022-01-20 23:59:04 -0500

    Frontend work, login/signup/home/dashboard

--- src/App.js
+++ src/App.js
@@ -8,30 +8,33 @@ import AwsPage from './Pages/AwsPage.js';
 import HomePage from './Pages/HomePage.js';
 import Unauthorized from './Pages/Unauthorized.js';
 import NotFound from './Pages/NotFound.js';
+import Login from './Pages/LoginPage.js';
+import Signup from './Pages/SignupPage.js';
 import { useSelector } from 'react-redux';
 
 function App() {
-
-  const user = useSelector(state => state.user.user);
+  const user = true; /* useSelector((state) => state.user.user); */
 
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Routes>
           <Route path='/' element={<HomePage />} />
-          { user ?
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          {user ? (
             <>
               <Route path='/azure' element={<AzurePage />} />
               <Route path='/aws' element={<AwsPage />} />
               <Route path='/google' element={<GooglePage />} />
             </>
-          : 
+          ) : (
             <>
               <Route path='/azure' element={<Unauthorized />} />
               <Route path='/aws' element={<Unauthorized />} />
               <Route path='/google' element={<Unauthorized />} />
             </>
-          }
+          )}
           <Route path='/unauthorized' element={<Unauthorized />} />
           <Route path='/*' element={<NotFound />} />
         </Routes>
--- src/Pages/AzurePage.js
+++ src/Pages/AzurePage.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function AzurePage() {
-  return <div><p>hi from the azure page</p></div>;
+  return (
+    <div className='h-screen w-full bg-[#363740] flex'>
+      <div className='open relative left-0 transition-all hover:transition-all ease-in-out h-screen bg-white'>
+        <p className='text-white'>Sidebar</p>
+      </div>
+      <div className='flex-grow w-full'>
+        <p className='text-white'>Main Dashboard</p>
+      </div>
+    </div>
+  );
 }
 
 export default AzurePage;
