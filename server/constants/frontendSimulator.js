b19d2af55657e671afafcf3b43e884a9f0708117
wdoconnell <91283923+wdoconnell@users.noreply.github.com>
2022-01-21 21:08:23 -0500

    first draft of 3-api functional backend, with mocked data from frontend. requires .env file to be placed in root directory.

---
+++ server/constants/frontendSimulator.js
@@ -0,0 +1,96 @@
+const MSWebSimulator = {
+  Requests: false,
+  BytesReceived: false,
+  BytesSent: false,
+  Http101: false,
+  Http2xx: false,
+  Http3xx: false,
+  Http401: false,
+  Http403: false,
+  Http404: false,
+  Http406: false,
+  Http4xx: false,
+  Http5xx: false,
+  MemoryWorkingSet: false,
+  AverageMemoryWorkingSet: false,
+  AverageResponseTime: false,
+  HttpResponseTime: false,
+  FunctionExecutionUnits: false,
+  FunctionExecutionCount: true,
+  AppConnections: false,
+  Handles: false,
+  Threads: false,
+  PrivateBytes: false,
+  IoReadBytesPerSecond: false,
+  IoWriteBytesPerSecond: false,
+  IoOtherBytesPerSecond: false,
+  RequestsInApplicationQueue: false,
+  CurrentAssemblies: false,
+  TotalAppDomains: false,
+  TotalAppDomainsUnloaded: false,
+  Gen0Collections: false,
+  Gen1Collections: false,
+  Gen2Collections: false,
+  HealthCheckStatus: false,
+  FileSystemUsage: false,
+};
+
+const MSStorageSimulator = {
+  UsedCapacity: false,
+  Transactions: false,
+  Ingress: false,
+  Egress: false,
+  SuccessServerLatency: false,
+  SuccessE2ELatency: false,
+  Availability: true,
+};
+
+const MSInsightsSimulator = {
+  availabilityResults: {
+    count: false,
+    duration: false,
+  },
+  browserTimings: {
+    networkDuration: false,
+    processingDuration: false,
+    receiveDuration: false,
+    sendDuration: false,
+    totalDuration: false,
+  },
+  dependencies: {
+    count: false,
+    duration: false,
+    failed: false,
+  },
+  pageViews: {
+    count: false,
+    duration: false,
+  },
+  performanceCounters: {
+    requestExecutionTime: false,
+    requestsInQueue: false,
+    requestsPerSecond: false,
+    exceptionsPerSecond: false,
+    processIOBytesPerSecond: false,
+    processCpuPercentage: false,
+    processorCpuPercentage: false,
+    memoryAvailableBytes: false,
+    processPrivateBytes: false,
+  },
+  requests: {
+    duration: true,
+    count: true,
+    failed: false,
+    rate: false,
+  },
+  exceptions: {
+    count: false,
+    browser: false,
+    server: false,
+  },
+  traces: {
+    count: false,
+  },
+};
+
+export { MSWebSimulator, MSStorageSimulator, MSInsightsSimulator };
