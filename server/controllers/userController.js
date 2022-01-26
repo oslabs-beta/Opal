5e09cc97634c5a2b011ef6b332d9010ce3c4ab7c
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-01-27 01:53:01 +0200

    updated middleware funcs & add error handling

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,63 +1,95 @@
-import db from '../models/UserModel.js';
+import db from "../models/UserModel.js";
 
 const userController = {};
 
 userController.checkUserCred = async (req, res, next) => {
   const { Username, Email } = req.body;
 
-  const q = 'SELECT 1 FROM users WHERE Username = $1 OR Email = $2';
+  const q = "SELECT 1 FROM users WHERE Username = $1 OR Email = $2";
 
   const response = await db.query(q, [Username, Email]);
 
-  if (response.rows.length < 1) {
-    res.locals.check = false;
-  } else res.locals.check = true;
+  if (response.rows.length > 1) {
+    return next({message: {msg: 'The username or email is already in use',  errors: { email: true, username: true }, error: true }});
+  } 
 
   return next();
 };
 
 userController.addUserCred = async (req, res, next) => {
   const { Username, Email, FirstName, LastName, Password } = req.body;
-
-  if (res.locals.check === false) {
+  
     const q =
-      'INSERT INTO users (username, email, firstName, lastName, password) VALUES ($1, $2, $3, $4, $5)';
+      "INSERT INTO users (username, email, firstName, lastName, password) VALUES ($1, $2, $3, $4, $5)";
     await db.query(
       q,
       [Username, Email, FirstName, LastName, Password],
       (err, result) => {
         if (err) {
-          throw new Error(err);
+          console.log(err);
         } else {
+          console.log(result);
+          if (result.rows.length > 1) {
+            return next({ message: {msg: 'There was an error creating your account', errors: { all: true }, error: true }});
+          }
           res.locals.user = true;
+          res.locals.userInfo = result.rows[0];
           return next();
         }
       }
     );
-  } else return next({ message: 'Username or Email already in use' });
 };
 
 userController.login = async (req, res, next) => {
   const { User, Password } = req.body;
 
-  const q = 'SELECT * FROM users WHERE Username = $1 OR Email = $1';
+  const q = "SELECT * FROM users WHERE Username = $1 OR Email = $1";
   //query is not finding anything so account for that!
-  const response = await db.query(q, [User]);
+  try {
+    const response = await db.query(q, [User]);
 
-  if (response.rows.length < 1) return next((res.locals.user = false));
-  else {
-    if (User === response.rows[0].username || User === response.rows[0].email) {
-      if (Password === response.rows[0].password) {
-        res.locals.user = true;
-        return next();
+    if (response.rows.length < 1)
+      return next({
+        message: {
+          error: true,
+          msg: "The username or email you gave does not exist.",
+          errors: { user: true, password: true },
+        },
+        status: 404,
+      });
+    else {
+      if (
+        User === response.rows[0].username ||
+        User === response.rows[0].email
+      ) {
+        if (Password === response.rows[0].password) {
+          res.locals.user = true;
+          res.locals.userInfo = response.rows[0];
+          return next();
+        } else {
+          res.locals.user = false;
+          return next({
+            message: {
+              error: true,
+              msg: "email or password is incorrect.",
+              errors: { user: true, password: true },
+            },
+            status: 400,
+          });
+        }
       } else {
         res.locals.user = false;
-        return next();
+        return next({
+          message: {
+            error: true,
+            msg: "The username or email you gave does not exist",
+            errors: { user: true, password: true },
+          },
+        });
       }
-    } else {
-      res.locals.user = false;
-      return next();
     }
+  } catch (e) {
+    console.log(e);
   }
 };
 
