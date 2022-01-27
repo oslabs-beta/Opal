4fb7deffc5482195ae6e99ac6f143e5649793c0c
wdoconnell <91283923+wdoconnell@users.noreply.github.com>
2022-01-27 04:03:04 -0500

    final updates from pair programming on Jan. 27 to sdk controller functionality to implement promise.all

--- server/controllers/functionmetricscontroller.js
+++ server/controllers/functionmetricscontroller.js
@@ -2,9 +2,9 @@ import { config } from 'dotenv';
 // No longer need simulators for now.
 // Could possibly dispense with other identities.
 import { DefaultAzureCredential } from '@azure/identity';
-import { ResourceManagementClient } from '@azure/arm-resources';
-import { Durations, MetricsQueryClient } from '@azure/monitor-query';
-import { SubscriptionClient } from '@azure/arm-resources-subscriptions';
+//import { ResourceManagementClient } from '@azure/arm-resources';
+import { MetricsQueryClient } from '@azure/monitor-query';
+//import { SubscriptionClient } from '@azure/arm-resources-subscriptions';
 import { MSInsightsSimulator, MSStorageSimulator, MSWebSimulator } from '../constants/frontendSimulator.js';
 config();
 
@@ -14,22 +14,19 @@ const credential = new DefaultAzureCredential();
 const metricQuery = new MetricsQueryClient(credential);
 
 functionMetricsController.getMSWebMetrics = async (req, res, next) => {
+  const start = new Date();
   // functionMetricsController.getMetrics gets all MS Web apps from function apps
   // based on the criteria selected in frontendSimulator.js
   let metrics;
   const metricsObj = {};
-  console.log('in MSWebMetrics');
   if (res.locals.executionOnly === true) {
     // If user is only looking for function execution count, output that only.
     metrics = ['FunctionExecutionCount'];
   } else {
     // If use is looking for all metrics, get all metrics.
-    console.log('getting all metrics');
     metrics = functionMetricsController.generateMetric1D(MSWebSimulator);
     // Get all metrics.
   }
-  console.log('function apps');
-  console.log(res.locals.functionApps);
   const promiseArray = [];
   const idArray = [];
   for (let app of res.locals.functionApps) {
@@ -38,70 +35,99 @@ functionMetricsController.getMSWebMetrics = async (req, res, next) => {
         err: 'Resource ID must be set to fetch metrics for function.',
       });
     }
-    //const promise = new Promise((resolve, reject) => {
-      const query = metricQuery.queryResource(app.id, metrics, {
-        granularity: 'PT1H',
-        timespan: { duration: 'PT24H' },
-      });
+    const query = metricQuery.queryResource(app.id, metrics, {
+      granularity: 'PT1H',
+      timespan: { duration: 'PT24H' },
+    });
     promiseArray.push(query);
-    idArray.push(app.id);
-  };
+    idArray.push(app.name);
+  }
 
   Promise.all(promiseArray).then((queryResultArray) => {
-    console.log('results');
-    console.log(queryResultArray);
-    // each array element has a .name (app.id) and a respones (response).
     for (let i = 0; i < queryResultArray.length; i++) {
       metricsObj[idArray[i]] = queryResultArray[i];
     }
-    console.log("metricsObj", metricsObj);
     res.locals.webMetrics = metricsObj;
     return next();
   });
-  //Promise.all(promiseArray);
-    //.then((result) => (metricsObj[app.name] = result));
-  // promiseArray.push(result);
-  // metricsObj[app.name] = result;
-  // res.locals.webMetrics = metricsObj;
+
+  const end = new Date();
+  console.log('getting MS Web metrics took');
+  console.log(end - start);
+  console.log('milliseconds');
 };
 
 functionMetricsController.getMSInsightsMetrics = async (req, res, next) => {
+  const start = new Date();
   const metrics = functionMetricsController.generateMetric2D(MSInsightsSimulator).split(',');
   const metricsArray = [];
 
-  // Alma: on lines 71-73 of functionMetricsController, I changed the id being used to
-  // the 'insightId' associated with the function app.
-  // The other changes I made were (1) adding the 'insightId' to
-  // the data that is sent from the frontend to the backend (line 415 of SDKController)
-  // and (2) associating the 'insightId' with a function (line 93 of SDK Controller).
-  // This is pushing the right metrics into 'insightMetrics', but I have not hooked it up to send
-  // the data back to the client yet.
+  const promiseArray = [];
+  const nameArray = [];
 
-  for await (let resource of res.locals.functionApps) {
-    const name = resource.name;
+  for (const resource of res.locals.functionApps) {
+    // const name = resource.name;
     const resId = resource.insightId;
     if (!resId) {
       return next({
         err: 'Resource ID must be set to fetch metrics for resource.',
       });
     }
-    const result = await metricQuery.queryResource(resId, metrics, {
+    const result = metricQuery.queryResource(resId, metrics, {
       granularity: 'PT1H',
       timespan: { duration: 'PT24H' },
       //aggregations: ['Count']
     });
-    result.name = name;
-    metricsArray.push(result);
+    promiseArray.push(result);
+    nameArray.push(resource.name);
   }
 
-  console.log('here is the current function data on res.locals');
-  // res.locals.functionApps is an array of function app objects, each of which has: name, id, location, insightId
-  console.log(res.locals.functionApps);
-  res.locals.insightsMetrics = metricsArray;
-  console.log('here are the metrics from MS insights');
-  // res.locals.metrics is an array of metric objects, each of which has cost, namespace, metrics, timespan, resource, granularity
-  console.log(res.locals.insightsMetrics);
-  return next();
+  console.log('promiseArray', promiseArray);
+  console.log('nameArray', nameArray);
+  Promise.all(promiseArray)
+    .then((queryResultArray) => {
+      console.log('queryResultArray', queryResultArray);
+      for (let i = 0; i < queryResultArray.length; i++) {
+        queryResultArray[i].name = nameArray[i];
+        metricsArray.push(queryResultArray[i]);
+      }
+      // metricsArray.push(queryResultArray);
+      console.log('metricsArray', metricsArray);
+    })
+    .then(() => {
+      res.locals.insightsMetrics = metricsArray;
+      console.log('res.locals.insightsMetrics', res.locals.insightsMetrics);
+      const end = new Date();
+      console.log('Fetching MS insights metrics took');
+      console.log(end - start);
+      console.log('millisecnds');
+      return next();
+    });
+
+  // result.name = name;
+  // metricsArray.push(result);
+
+  // const promiseArray = [];
+  // const idArray = [];
+  // for (let app of res.locals.functionApps) {
+  //   if (!app.id) {
+  //     return next({
+  //       err: 'Resource ID must be set to fetch metrics for function.',
+  //     });
+  //   }
+  //     const query = metricQuery.queryResource(app.id, metrics, {
+  //       granularity: 'PT1H',
+  //       timespan: { duration: 'PT24H' },
+  //     });
+  //   promiseArray.push(query);
+  //   idArray.push(app.name);
+  // };
+
+  // Promise.all(promiseArray).then((queryResultArray) => {
+  //   for (let i = 0; i < queryResultArray.length; i++) {
+  //     metricsObj[idArray[i]] = queryResultArray[i];
+  //   }
+  //   res.locals.webMetrics = metricsObj;
 };
 
 // Storage metrics controller is not currently being used.
@@ -126,11 +152,6 @@ functionMetricsController.getStorageMetrics = async (req, res, next) => {
   return next();
 };
 
-// this is a duplicate of what appears in performancecontroller. Only need one of each.
-
-// functionMetricsController.generateMetric1D takes in a 1-level object identifying properties
-// that the user may want as metrics. It will output into an array only those metrics
-// that are currently set to 'true.'
 functionMetricsController.generateMetric1D = function (resObj) {
   let metricArray = [];
   for (let key in resObj) {
@@ -142,7 +163,6 @@ functionMetricsController.generateMetric1D = function (resObj) {
 };
 
 functionMetricsController.generateMetric2D = function (resObj) {
-  // outputs chosen metrics as a string.
   let metricString = '';
   for (let keyTopLevel in resObj) {
     for (let keyBottomLevel in resObj[keyTopLevel]) {
