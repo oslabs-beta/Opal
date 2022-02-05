85e854038779ac6e6d2cbb2d109873c6de8c55eb
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-05 09:23:30 +0200

    created type exports to map types to useSelector & useDispatch hooks

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice.js';
-import dashReducer from './slices/dashSlice.js';
-import awsReducer from './slices/awsSlice.js';
-import azureReducer from './slices/azureSlice.js';
+import userReducer from './slices/userSlice';
+import dashReducer from './slices/dashSlice';
+import awsReducer from './slices/awsSlice';
+import azureReducer from './slices/azureSlice';
 
 export const store = configureStore({
     reducer: {
@@ -12,3 +12,15 @@ export const store = configureStore({
         aws: awsReducer,
     }
 })
+
+/**
+ * @method { getState }
+ * @method { dispatch }
+ * 
+ * both exist as prototype methods on the configureStore store object
+*/
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {aws: awsState, dash: dashState, users: UsersState, azure: azureState}
+export type AppDispatch = typeof store.dispatch
