c0e60de5ad3ed04ff8dc4d8ef035d58792705988
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-05 08:42:37 +0200

    Created export Pages file to modularize imports into there respected files

---
+++ src/Pages/index.ts
@@ -0,0 +1,13 @@
+export { AzurePage } from './AzurePage';
+
+export { GooglePage} from './GooglePage';
+
+export { AwsPage } from './AwsPage';
+
+export { HomePage } from './HomePage';
+
+export { NotFoundPage } from './NotFoundPage';
+
+export { LoginPage } from './LoginPage';
+
+export { SignUpPage } from './SignUpPage';
