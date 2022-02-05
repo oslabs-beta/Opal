abffbdbc49484c927e0db873c3f7067c4e855c87
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-05 09:21:55 +0200

    changed & added new configurations to webpack (TS)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ['./src/index.tsx'],
   // Enable importing JS and TSX files without identifying their extension.
   resolve: {
     extensions: ['.js', '.jsx', '.tsx', '.ts'],
@@ -20,13 +20,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?/,
+        test: /\.(tsx|ts)$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
-      {
-        test: /|.(ts|tsx)$/,
-        loader: 'awesome-typescript-loader'
       },
       {
         test: /\.(js|jsx)$/,
