414d108ae0f2deae33699e20179c2b299bd1eae1
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-05 09:15:19 +0200

    Added new page component to all users to edit/add new opal account information or edit/add new azure account information

---
+++ src/Components/AccountPageComponent.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const AccountPage = () => {
+  return (
+    <div className="text-2xl p-20">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laborum temporibus tempora sunt magnam, esse debitis fugit repellendus cupiditate minus odit alias officia vero eveniet inventore deleniti. Inventore, in animi!</h1>
+      <h1>
+        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, iste amet, doloremque repellendus ipsa nobis eveniet esse, nemo quis explicabo laboriosam iure sapiente. Pariatur harum placeat excepturi aut vero magnam.</div>
+        <div>Nemo qui accusantium labore repellendus numquam, saepe sit soluta sint quidem impedit est praesentium iure deserunt laudantium explicabo tempore omnis aspernatur? Cum et recusandae minima ea distinctio eaque cumque! Minus?</div>
+        <div>Obcaecati commodi ullam molestiae esse quam, incidunt magnam quisquam, minima ducimus doloremque eos voluptatibus qui libero tempore, sint dolores. Ipsa qui exercitationem delectus quis culpa architecto cumque sunt earum illo?</div>
+        <div>Consectetur libero commodi nostrum quidem? Ea esse dicta, dolore quod reiciendis fuga eligendi, nobis nam doloremque id mollitia! Architecto maxime alias animi minus dignissimos voluptatum autem eveniet voluptates eligendi laudantium!</div>
+        <div>Voluptate tenetur itaque atque. Temporibus, id cupiditate. Ipsum assumenda enim earum aliquid eum ratione natus dolor saepe magnam delectus. Sequi accusamus, asperiores veritatis obcaecati excepturi dolores reiciendis repellendus fugiat praesentium.</div>
+        <div>Molestiae necessitatibus autem, nihil voluptatibus beatae cumque ullam aspernatur nostrum impedit illo doloribus! Nisi eaque, sed sapiente similique, distinctio ab laudantium pariatur asperiores fugiat perferendis illum! Nemo fugiat ipsa magnam.</div>
+        <div>Aspernatur, nobis magnam, corrupti quod sequi dolore necessitatibus amet placeat laboriosam consequuntur cupiditate, ullam libero nulla quam fugiat eius suscipit rerum. Aliquam nemo deserunt veniam odio! Nisi repellendus dolorem ab.</div>
+        <div>Doloribus quis tenetur itaque porro cupiditate corrupti molestias consectetur non ab soluta necessitatibus beatae, delectus repellendus accusantium eveniet facere debitis deserunt? Odio sint ratione dolor sapiente totam corporis officiis delectus.</div>
+        <div>Aliquid mollitia, dignissimos excepturi distinctio earum accusamus, voluptatum voluptate possimus natus quibusdam est magni sunt fugiat fugit laudantium quae velit. Eius optio et explicabo amet, harum suscipit voluptatibus adipisci pariatur.</div>
+        <div>Placeat quibusdam inventore excepturi asperiores tempora dolorem ab. Quos, fugiat, excepturi delectus cumque hic illum, ipsum quis odio consectetur perspiciatis labore deleniti dignissimos recusandae necessitatibus commodi quasi totam. Quis, provident!</div>
+      </h1>
+    </div>
+  );
+}
