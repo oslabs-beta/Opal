ad8e5f1e811ce04701be929dee3cf610cc400de9
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-05 20:55:05 +0200

    Added loading state to fetch data from the users azure account & fixes navigation

--- src/Components/OverviewPageComponent.tsx
+++ src/Components/OverviewPageComponent.tsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Graph } from '.';
-// import { useAppDispatch } from '../redux/hooks';
-import { changeTab } from '../redux/slices/dashSlice';
-import { getExecOnlyData } from '../util/getExecOnlyData';
-import { getMoreData } from '../util/getMoreData';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Graph, Loader } from ".";
+import { getExecOnlyData } from "../util/getExecOnlyData";
 
 export const OverviewPage = () => {
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
 
   let session;
 
-  if (sessionStorage.getItem('graphs'))
-    session = JSON.parse(sessionStorage.getItem('graphs') || '');
+  if (sessionStorage.getItem("graphs"))
+    session = JSON.parse(sessionStorage.getItem("graphs") || "");
 
   const [data, setData] = useState(session || []);
   const graphArr = [];
 
   useEffect(() => {
-    if (!sessionStorage.getItem('graphs')) {
-      const data:any = getExecOnlyData();
-      console.log('getExecOnlyData',data);
+    if (!sessionStorage.getItem("graphs")) {
+      setLoading(true);
+      const data: any = getExecOnlyData();
+      // console.log("getExecOnlyData", data);
       Promise.resolve(data)
-        .then((result:object) => {
-          console.log(result);
+        .then((result: object) => {
+          setLoading(false);
           if (result) {
             for (let i in result) {
               for (let x in result[i]) {
@@ -35,34 +35,39 @@ export const OverviewPage = () => {
             }
           }
 
-          sessionStorage.setItem('graphs', JSON.stringify(graphArr));
+          sessionStorage.setItem("graphs", JSON.stringify(graphArr));
           setData(graphArr);
         })
         .catch((err) => console.log(err));
-
     }
   }, []);
 
-  return (<div>
-      <div className='flex flex-wrap w-full justify-center items-center'>
-          {console.log('specific data ')}
-          {data && data.map((d) => {
+  return (
+    <>
+      {loading ? (
+        <Loader theme={'azure'} />
+      ) : (
+        <div>
+          <div className="flex flex-wrap w-full justify-center items-center">
+            {data &&
+              data.map((d) => {
                 return (
                   <div
                     key={d.id}
-                    className='flex flex-col items-center justify-center w-2/5 mb-52 p-4 border-2 border-gray-300 border-opacity-20 rounded-lg ml-4 mr-4 shadow-2xl'
-                    onClick={() => {
-                      Promise.resolve(getMoreData(d)).then((data) => {
-                        dispatch(changeTab(`FunctionApp ${data.name}`));
-                      });
-                    }}
+                    className="flex flex-col items-center justify-center w-2/5 mb-52 p-4 border-2 border-gray-300 border-opacity-20 rounded-lg ml-4 mr-4 shadow-2xl"
+                    onClick={() =>
+                      navigate(`/azure/functionApp/${d.name}`, { state: d })
+                    }
                   >
-                    <h1 className='text-4xl font-bold mb-14'>{d.name}</h1>
-                    <h3 className='text-2xl mb-12'>{d.metricName}</h3>
-                    <Graph data={d} format={'1h'} />
+                    <h1 className="text-4xl font-bold mb-14">{d.name}</h1>
+                    <h3 className="text-2xl mb-12">{d.metricName}</h3>
+                    <Graph data={d} format={"1h"} />
                   </div>
                 );
               })}
+          </div>
         </div>
-  </div>)
-}
+      )}
+    </>
+  );
+};
