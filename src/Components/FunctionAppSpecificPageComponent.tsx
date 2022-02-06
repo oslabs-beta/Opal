9d9b7c6da2e29890e7b9052e7a9c20531fad6d0c
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-06 10:59:20 +0200

    Added functionality to fetch a function apps functions & navigate to metrics about that specific function

--- src/Components/FunctionAppSpecificPageComponent.tsx
+++ src/Components/FunctionAppSpecificPageComponent.tsx
@@ -1,27 +1,128 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Loader } from ".";
-import { getMoreData } from "../util/getMoreData";
+import { getFuncAppData } from "../util/getFuncAppData";
+import { getFuncAppFunctions } from "../util/getFuncAppFunctions";
+import { XCircleIcon, CheckCircleIcon } from "@heroicons/react/solid";
+import { motion } from "framer-motion";
+
+interface SpecificFuncData {
+  name: string;
+  id: string;
+  location: string;
+  resourceGroupId: string;
+  metric: Array<object>;
+}
+
+interface LocationObj {
+  state: any;
+}
 
 export const FunctionAppSpecificPage = () => {
-  const location = useLocation();
+  const location: LocationObj = useLocation();
   const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState<boolean | null>(null);
+
+  const [data, setData] = useState<SpecificFuncData | null>(null);
+  const [functions, setFunctions] = useState<[] | null>(null);
+
+  const resourceGroupId = location.state.resourceGroupId.split("/");
+  const resourceGroupName = resourceGroupId[resourceGroupId.length - 1];
+
   useEffect(() => {
     setLoading(true);
-    Promise.resolve(getMoreData(location.state)).then((res) => {
-      setLoading(false);
-      console.log(res);
-    });
-  }, [location]);
+    Promise.resolve(getFuncAppData(location.state))
+      .then((res: SpecificFuncData) => {
+        // setLoading(false);
+        setData(res);
+      })
+      .then((_) => {
+        Promise.resolve(
+          getFuncAppFunctions({
+            subscriptionId: location.state.subscriptionId,
+            resourceGroupName,
+            appName: location.state.name,
+          })
+        ).then((res: any) => {
+          setLoading(false);
+          setFunctions(res.value);
+        });
+      });
+  }, [location, resourceGroupName]);
+
+  console.log(functions);
 
   return (
     <>
       {loading ? (
-        <Loader theme='azure' />
-      ) : (
-        <div>
-          <h1>Oh so you think you can view a specific function app yeh?</h1>
+        <Loader theme="azure" />
+      ) : data ? (
+        <div className="w-full flex justify-center mb-16">
+          <div className="w-11/12">
+            <h1 className='text-2xl'>{data!.name}</h1>
+            <br /><br />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Molestias fugiat ab dolorem debitis aliquam sit numquam esse iure
+              enim cum praesentium, facere veniam modi necessitatibus, expedita
+              sunt laborum, porro officia!
+            </p>
+
+            <br />
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis veniam iusto asperiores quibusdam laboriosam nesciunt et ex consequatur voluptatem delectus a maiores cupiditate maxime dignissimos, amet repellendus natus. Commodi, non?</p>
+            <br />
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nobis necessitatibus, harum officia aspernatur cumque numquam odit sint tempore ut aperiam nesciunt corrupti, repudiandae fugit provident iure amet ad explicabo?</p>
+            <br />
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nobis necessitatibus, harum officia aspernatur cumque numquam odit sint tempore ut aperiam nesciunt corrupti, repudiandae fugit provident iure amet ad explicabo?</p>
+            <br />
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nobis necessitatibus, harum officia aspernatur cumque numquam odit sint tempore ut aperiam nesciunt corrupti, repudiandae fugit provident iure amet ad explicabo?</p>
+            <br />
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nobis necessitatibus, harum officia aspernatur cumque numquam odit sint tempore ut aperiam nesciunt corrupti, repudiandae fugit provident iure amet ad explicabo?</p>
+            <br /><br />
+            <div className="flex h-auto w-full">
+              <div className="h-full p-5 border-t-2 border-black text-xl font-semibold">
+                <h1>Functions</h1>
+              </div>
+              <div className="h-full flex-grow border-l-2 border-t-2 border-black p-5">
+                {functions &&
+                  functions.map((func: any) => {
+                    return (
+                      <motion.div
+                        whileHover={{scale: 1.025}}
+                        key={func.name}
+                        className="p-6 mb-2 shadow-md border-2 rounded-lg flex justify-between cursor-pointer hover:bg-[#e5e7eb] hover:shadow-xl"
+                      >
+                        <h1>{func.properties.name}</h1>
+                        <div>
+                          {func.properties.isDisabled ? (
+                            <div className="flex space-x-4 items-center">
+                              <h1 className=" font-medium">Disabled</h1>
+                              <XCircleIcon className="w-6 h-6 text-red-500" />
+                            </div>
+                          ) : (
+                            <div className="flex space-x-10 items-center justify-between ">
+                              <h1>{ func.properties.language }</h1>
+                              <div className="flex space-x-4 items-center ">
+                                <h1 className=" font-medium">Active</h1>
+                                <CheckCircleIcon className="w-6 h-6 text-green-500" />
+                              </div>
+                            </div>
+                          )}
+                        </div>
+                      </motion.div>
+                    );
+                  })}
+              </div>
+            </div>
+          </div>
         </div>
+      ) : (
+        ""
       )}
     </>
   );
