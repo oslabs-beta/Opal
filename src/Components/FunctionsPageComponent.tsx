c02569435507db17f88b28d32dee48be81b80112
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-08 20:17:30 +0200

    Set up functions list page to use the session storage set by the overview component

--- src/Components/FunctionsPageComponent.tsx
+++ src/Components/FunctionsPageComponent.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const FunctionsPage = () => {
+  const [data, setData] = useState(JSON.parse(sessionStorage.getItem('functions') || ''));
+
+  console.log(data);
+  
   return (
       <div>
           <h1>List of all functions in a function app</h1>
