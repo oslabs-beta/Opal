343b1bd0157c1123e50e80a3868290b893e1179a
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-08 20:12:00 +0200

    Updating the util function to send the correct data to the backend

--- src/util/getAllFuncs.ts
+++ src/util/getAllFuncs.ts
@@ -9,20 +9,23 @@ const baseUrl = `http://localhost:3000/getAllFunctions`;
  * @returns a metric data collection
  */
 
-export const getFuncAppFunctions = async ({
-  executionObj
-}) => {
+export const getAllFunctions = async ({ executionObj }) => {
+  // console.log(executionObj);
   try {
     const postObj = {};
     for (let sub in executionObj) {
       postObj[sub] = {};
       for (let resourceGroup in executionObj[sub]) {
         postObj[sub][resourceGroup] = [];
-        for (let resource = 0; resource < executionObj[sub][resourceGroup].length; resource++) {
-          postObj[sub][resourceGroup].push(executionObj[sub][resourceGroup][resource]);
+        // console.log(executionObj[sub][resourceGroup]);
+        for (let resource in executionObj[sub][resourceGroup]) {
+          postObj[sub][resourceGroup].push(
+            executionObj[sub][resourceGroup][resource].name
+          );
         }
       }
     }
+    console.log(JSON.stringify(postObj));
     const response = await axios.post(baseUrl, postObj);
     return response.data;
   } catch (err) {
