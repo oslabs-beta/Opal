1800e26e78c5fb7b30f6cc1db2d9881b1b5c1fc2
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-09 07:02:55 +0200

    Added JSDoc code documentation for the util function

--- src/util/getFuncDetails.ts
+++ src/util/getFuncDetails.ts
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:3000/getSpecificFunctionMetrics";
 
+/**
+ * 
+ * @param data { workSpaceId, functionName }
+ * @function async to fetch all information about a specific function in there users account
+ * @returns an array of all information pertaining to a specific function 
+ */
 
 export const getFuncDetails = async (data) => {
   try {
