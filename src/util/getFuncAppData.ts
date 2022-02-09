a3e8b8e3d5b76a924a3fe7e37247cc93143df792
wdoconnell <91283923+wdoconnell@users.noreply.github.com>
2022-02-08 22:06:27 -0500

    Update Specific Function App Page to permit selection of granularity and time period. Update server controllers to handle granularity and time period in corresponding fashion.

--- src/util/getFuncAppData.ts
+++ src/util/getFuncAppData.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 // We should be able to use the proxy url to get this without referencing localhost:3000.
 const baseUrl = 'http://localhost:3000/getAppDetails';
 
-export const getFuncAppData = async (data) => {
+export const getFuncAppData = async (data, timespan = 1, granularity = 3) => {
   try {
     const result = await axios.post(baseUrl, {
       id: data.id,
@@ -11,6 +11,8 @@ export const getFuncAppData = async (data) => {
       insightId: data.insightId,
       resourceGroupName: data.resourceGroupName,
       resourceGroupId: data.resourceGroupId,
+      timespan: timespan,
+      granularity: granularity
     });
     return result.data;
   } catch (err) {
