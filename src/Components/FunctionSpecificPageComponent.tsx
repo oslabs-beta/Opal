2319cfec7f59827884c28310d430aa9c543d12d8
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-09 07:13:32 +0200

    modularised the component to use the newly created FuncListComponent

--- src/Components/FunctionSpecificPageComponent.tsx
+++ src/Components/FunctionSpecificPageComponent.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Loader } from ".";
+import { getFuncDetails } from "../util/getFuncDetails";
+
+interface LocationObj {
+  state: any;
+}
 
 export const FunctionSpecificPage = () => {
+  const location: LocationObj = useLocation();
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<null | object>(null);
+
+  const workSpaceId = sessionStorage.getItem("workSpaceId");
+  console.log(location, workSpaceId);
+  useEffect(() => {
+    const details: any = getFuncDetails({
+      workSpaceId,
+      functionName: location.state.properties.name,
+    });
+    console.log(details);
+    Promise.resolve(details).then((res) => {
+      setLoading(false);
+      console.log(res);
+    });
+  });
   return (
-      <div>
+    <>
+      { loading ? 
+        <Loader theme='azure' />
+        :
+        <div>
           <h1> Oh so you want to view a specific function </h1>
-      </div>
+        </div>
+      }
+    </>
   );
-}
+};
