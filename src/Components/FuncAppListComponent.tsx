b49bb2310d1b509346999468f868356816480cf9
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-10 05:26:48 +0200

    reformated the data being display

--- src/Components/FuncAppListComponent.tsx
+++ src/Components/FuncAppListComponent.tsx
@@ -10,16 +10,14 @@ interface Props {
 export const FuncAppListComponent = ({ data }: Props) => {
   const navigate = useNavigate();
 
-  console.log(data)
-
   return (
     <motion.div whileHover={{scale: 1.025}} key={data.name} className='p-6 mb-4 shadow-lg cursor-pointer border-2 rounded-lg flex justify-between hover:bg-[#e5e7eb] hover:shadow-xl' onClick={() =>
       navigate(`/azure/functionApp/${data.name}`, { state: data })
     }>
       <h1>{data.name}</h1>
       <div className='flex space-x-4'>
+        <h1>{data.resourceGroupName}</h1>
         <h1>{data.subscriptionDisplayName}</h1>
-        <h1>Total Invocation: {data.totalCount}</h1>
       </div>
     </motion.div>
   );
