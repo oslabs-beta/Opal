a3f4c8c732f4c0fa28c1fdb413a367aa4d995292
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-10 03:48:09 +0200

    updated styling and added additional information

--- src/Components/FuncAppListComponent.tsx
+++ src/Components/FuncAppListComponent.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
+import { FunctionApp } from '../../Types';
 
-export const FuncAppListComponent = ({ data }) => {
+interface Props {
+  data: FunctionApp
+}
+
+export const FuncAppListComponent = ({ data }: Props) => {
   const navigate = useNavigate();
+
+  console.log(data)
 
   return (
     <motion.div whileHover={{scale: 1.025}} key={data.name} className='p-6 mb-4 shadow-lg cursor-pointer border-2 rounded-lg flex justify-between hover:bg-[#e5e7eb] hover:shadow-xl' onClick={() =>
       navigate(`/azure/functionApp/${data.name}`, { state: data })
     }>
       <h1>{data.name}</h1>
-      <div>
+      <div className='flex space-x-4'>
         <h1>{data.subscriptionDisplayName}</h1>
+        <h1>Total Invocation: {data.totalCount}</h1>
       </div>
     </motion.div>
   );
