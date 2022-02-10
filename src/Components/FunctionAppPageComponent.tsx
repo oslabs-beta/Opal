e209c53bc52c406e024596f07eced68d56970aa0
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2022-02-10 03:47:33 +0200

    added a view to view all funcationApps and prepared a filter to filter the funcApps based on a search query

--- src/Components/FunctionAppPageComponent.tsx
+++ src/Components/FunctionAppPageComponent.tsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
-import { FuncAppListComponent } from '.';
+import React, { useEffect, useState } from "react";
+import { FuncAppListComponent } from ".";
 
 export const FunctionAppPage = () => {
-  const [data, setData] = useState(JSON.parse(sessionStorage.getItem('graphs') || "{}"));
+  const [data, setData] = useState(
+    JSON.parse(sessionStorage.getItem("graphs") || "{}")
+  );
+  const [filtered, setFiltered] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  console.log(data);
+  useEffect(() => {
+    
+  })
 
   return (
     <div className="w-full h-auto flex justify-center">
       <div className="w-11/12">
-        {data?.length > 0 && data.map((val:any) => {
-          return <FuncAppListComponent key={val.name} data={val} />
-        })}
+        {data?.length > 0 && searchQuery.length === 0
+          ? data.map((val: any) => {
+              return <FuncAppListComponent key={val.name} data={val} />;
+            })
+          : filtered?.map((val: any) => {
+              return <FuncAppListComponent key={val.name} data={val} />;
+            })}
       </div>
     </div>
   );
