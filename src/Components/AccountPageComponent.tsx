c35c54acecc542aaf0f4d28566144a72667d9939
Hussein Hamade <h.hamade98@gmail.com>
2022-02-10 11:46:27 -0500

    AccountPageComponent functionality pt1

--- src/Components/AccountPageComponent.tsx
+++ src/Components/AccountPageComponent.tsx
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useAppSelector } from "../redux/hooks";
+import { motion } from 'framer-motion';
+import axios from "axios";
+import { refObj } from "../../Types";
 
 export const AccountPage = () => {
   const user = useAppSelector((state) => state.user.user);
-  console.log(user);
+
+  const Firstname = useRef<refObj>(null);
+  const Lastname = useRef<refObj>(null);
+  const Username = useRef<refObj>(null);
+  const Email = useRef<refObj>(null);
+  const Password = useRef<refObj>(null);
+  const ConfirmPass = useRef<refObj>(null);
+
+  // interface Query {
+  //   Firstname: Firstname.current.value | null;
+  //   Lastname: Lastname.current.value | null;
+  //   Username: Username.current.value | null;
+  //   Email: Email.current.value | null;
+  //   Password: Password.current.value | null;
+  //   ConfirmPass: ConfirmPass.current.value | null;
+  // }
+
+  const updateCred = axios.post('/user/update', {Username})
   return (
     <div className="w-full flex justify-center">
-      <div className="w-11/12 mb-20">
+      <div className="w-11/12 mb-20 flex flex-col justify-center items-center">
         <h1 className="text-blue-500 text-2xl mb-4">Account Details</h1>
         <div className="mb-10">
           <h1 className="text-xl mb-4">Current Opal Account Information</h1>
-          <div className="flex justify-between w-1/3">
+          <div className="flex justify-between w-full">
             <h2 className="">Username :</h2>
             <h2 className="">{user.username}</h2>
           </div>
-          <div className="flex justify-between w-1/3">
-            <h2 className="">First Name :</h2>
+          <div className="flex justify-between w-full">
+            <h2 className="">First Name: </h2>
             <h2 className="">{user.firstname}</h2>
           </div>
-          <div className="flex justify-between w-1/3">
+          <div className="flex justify-between w-full">
             <h2 className="">Last Name :</h2>
             <h2 className="">{user.lastname}</h2>
           </div>
-          <div className="flex justify-between w-1/3">
+          <div className="flex justify-between w-full">
             <h2 className="">Email :</h2>
             <h2 className="">{user.email}</h2>
-          </div>
-          <div className="flex justify-between w-1/3">
-            <h2 className="">Password :</h2>
-            <h2 className="">{user.password}</h2>
           </div>
         </div>
         <div className="flex w-full justify-center items-center">
@@ -42,6 +58,7 @@ export const AccountPage = () => {
               <div className='flex flex-col p-2'>
                 <label className='mb-2' htmlFor="">First Name</label>
                 <input
+                  ref={Firstname}
                   placeholder="Enter your first name..."
                   className="border-2 border-sky-500 py-3 px-4 rounded-lg"
                   type="text"
@@ -50,6 +67,7 @@ export const AccountPage = () => {
               <div className='flex flex-col p-2'>
                 <label className='mb-2' htmlFor="">Last Name</label>
                 <input
+                  ref={Lastname}
                   placeholder="Enter your last name..."
                   className="border-2 border-sky-500 py-3 px-4 rounded-lg"
                   type="text"
@@ -58,6 +76,7 @@ export const AccountPage = () => {
               <div className='flex flex-col p-2'>
                 <label className='mb-2' htmlFor="">Username</label>
                 <input
+                  ref={Username}
                   placeholder="Enter your username..."
                   className="border-2 border-sky-500 py-3 px-4 rounded-lg"
                   type="text"
@@ -66,22 +85,34 @@ export const AccountPage = () => {
               <div className='flex flex-col p-2'>
                 <label className='mb-2' htmlFor="">Email</label>
                 <input
+                  ref={Email}
                   placeholder="Enter your email..."
                   className="border-2 border-sky-500 py-3 px-4 rounded-lg"
                   type="text"
                 />
               </div>
               <div className='flex flex-col p-2'>
-                <label className='mb-2' htmlFor=""> Password</label>
+                <label className='mb-2' htmlFor="">Password</label>
                 <input
+                  ref={Password}
                   placeholder="Enter your password..."
                   className="border-2 border-sky-500 py-3 px-4 rounded-lg"
                   type="text"
                 />
               </div>
-              <button className="bg-sky-500 mt-4 mb-4 w-full p-4 rounded-lg text-white">
+              <div className='flex flex-col p-2'>
+                <label className='mb-2' htmlFor="">Confirm Password</label>
+                <input
+                  ref={ConfirmPass}
+                  placeholder="Confirm your password..."
+                  className="border-2 border-sky-500 py-3 px-4 rounded-lg"
+                  type="text"
+                />
+              </div>
+              {/* @ts-ignore */}
+              <motion.button className="bg-sky-500 mt-4 mb-4 w-full p-4 rounded-lg text-white" whileTap={{ scale: 0.9 }} animate={{ y: 10 }} onClick={() => console.log(Firstname.current.value, Lastname.current.value, Email.current.value, Username.current.value, Passwor!.current.value, ConfirmPass.current.value) }>
                 Submit
-              </button>
+              </motion.button>
             </div>
           </div>
         </div>
